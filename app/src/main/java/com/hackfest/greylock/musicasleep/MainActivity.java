fb28fb33539b363172ca501ce04cd9d15bab683a
mjp2220 <mjp2220@columbia.edu>
2014-07-19 16:30:41 -0700

    Added functionality so we can fetch data

--- app/src/main/java/com/hackfest/greylock/musicasleep/MainActivity.java
+++ app/src/main/java/com/hackfest/greylock/musicasleep/MainActivity.java
@@ -5,10 +5,12 @@ package com.hackfest.greylock.musicasleep;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.spotify.sdk.android.Spotify;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
@@ -17,15 +19,33 @@ import com.spotify.sdk.android.playback.ConnectionStateCallback;
 import com.spotify.sdk.android.playback.Player;
 import com.spotify.sdk.android.playback.PlayerNotificationCallback;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends Activity implements
         PlayerNotificationCallback, ConnectionStateCallback {
 
-    // TODO: Replace with your client ID
     private static final String CLIENT_ID = "3acf3492794d499a87be2120198d616c";
-    // TODO: Replace with your redirect URI
     private static final String REDIRECT_URI = "music-asleep-login://callback";
+    private static final String DEFAULT_TRACK_ID = "2TpxZ7JUBn3uw46aR7qd6V";
+    private static final String OTHER_TRACK_ID = "6NmXV4o6bmp704aPGyTVVG";
+    private static final String TRACK_PREFIX = "spotify:track:";
+    private static final String SPOTIFY_TRACK_QUERY = "https://api.spotify.com/v1/tracks/";
 
     private Button nextSongButton;
+    private TextView songName;
+    private TextView artistName;
 
     private Player mPlayer;
     @Override
@@ -48,7 +68,7 @@ public class MainActivity extends Activity implements
                 public void onInitialized() {
                     mPlayer.addConnectionStateCallback(MainActivity.this);
                     mPlayer.addPlayerNotificationCallback(MainActivity.this);
-                    mPlayer.play("spotify:track:2TpxZ7JUBn3uw46aR7qd6V");
+                    playSongAndSetData(DEFAULT_TRACK_ID);
                 }
 
                 @Override
@@ -57,10 +77,12 @@ public class MainActivity extends Activity implements
                 }
             });
             nextSongButton = (Button) findViewById(R.id.next_song_button);
+            songName = (TextView) findViewById(R.id.song_name);
+            artistName = (TextView) findViewById(R.id.artist_name);
             nextSongButton.setEnabled(true);
             nextSongButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                    mPlayer.play("spotify:track:3zBhJBEbDD4a4SO1EaEiBP");
+                    playSongAndSetData(OTHER_TRACK_ID);
                 }
             });
         }
@@ -99,5 +121,60 @@ public class MainActivity extends Activity implements
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    public void playSongAndSetData(String trackId) {
+        mPlayer.play(getTrackUri(trackId));
+        String trackRequest = SPOTIFY_TRACK_QUERY + trackId;
+        new RESTfulAPIService().execute(trackRequest);
+    }
+
+    public String getTrackUri(String trackId) {
+        return TRACK_PREFIX + trackId;
+    }
+
+    private class RESTfulAPIService extends AsyncTask<String, Void, String> {
+        protected String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
+            InputStream in = entity.getContent();
+            StringBuffer out = new StringBuffer();
+            int byteCount = 1;
+            while (byteCount > 0) {
+                byte[] bytesRead = new byte[4096];
+                byteCount =  in.read(bytesRead);
+                if (byteCount > 0) {
+                    out.append(new String(bytesRead, 0, byteCount));
+                }
+            }
+            return out.toString();
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            String urlParam = params[0];
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpContext localContext = new BasicHttpContext();
+            HttpGet httpGet = new HttpGet(urlParam);
+            String textResponse = null;
+            try {
+                HttpResponse response = httpClient.execute(httpGet, localContext);
+                HttpEntity entity = response.getEntity();
+                textResponse = getASCIIContentFromEntity(entity);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return textResponse;
+        }
+        @Override
+        protected void onPostExecute(String results) {
+            if (results != null) {
+                try {
+                    JSONObject responseJson = new JSONObject(results);
+                    songName.setText(responseJson.getString("name"));
+                    JSONArray artists = responseJson.getJSONArray("artists");
+                    artistName.setText(((JSONObject)artists.get(0)).getString("name"));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
