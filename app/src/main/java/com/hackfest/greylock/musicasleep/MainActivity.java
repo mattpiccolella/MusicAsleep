83f5ee7f62a007e9c56f8b944dd9fb0636e56833
Zachary Lawrence <zacharyclawrence@gmail.com>
2014-07-20 08:01:38 -0700

    Removed unnecessary imports

--- app/src/main/java/com/hackfest/greylock/musicasleep/MainActivity.java
+++ app/src/main/java/com/hackfest/greylock/musicasleep/MainActivity.java
@@ -17,8 +17,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.spotify.sdk.android.Spotify;
